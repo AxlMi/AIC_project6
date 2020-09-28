34f45af7f5ab3d37f3ec5f110017fb1b92dd3f01
AxlMi <47877419+AxlMi@users.noreply.github.com>
2020-09-28 02:22:53 +0200

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -7,6 +7,7 @@ from email.mime.text import MIMEText
 import socket
 import var
 
+"""I connect to the SMTP server thanks to the different variables filled in the confidential.py file"""
 msg = MIMEMultipart()
 
 msg['From'] = confidential.fromaddr
@@ -23,11 +24,13 @@ toaddrs = confidential.toaddrs
 name = socket.gethostname()
 
 def send_ok():
+    """ this function will send this message if there was no error during the execution of the script"""
     messages = MIMEText('Bonjour,\nla sauvegarde du {} concernant le serveur {} à fonctionné'.format(var.today_date, name))
     msg.attach(messages)
     server.sendmail(fromaddr, toaddrs, msg.as_string())
 
 def send_err(error,):
+    """ this function will send this message if we encounter an error """
     messages = MIMEText("Bonjour,\n\nla sauvegarde du {} concernant le serveur {} à rencontré l'erreur suivante :\n{}".format(var.today_date, name, error))
     msg.attach(messages)
     server.sendmail(fromaddr, toaddrs, msg.as_string())
