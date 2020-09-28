2499be3d8c773d23aa7ecc4f81a4ec76f5e300a5
AxlMi <47877419+AxlMi@users.noreply.github.com>
2020-09-28 02:19:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 import create_crontab
 import send_mail
 
-
+"""if all goes well, I will send an email to indicate the correct execution of the script"""
 try:
     # instantiate DB
     dbase = sqlsave.Database(confidential.sql_user, confidential.sql_password)
@@ -25,10 +25,11 @@ try:
         send_mail.send_ok()
     except Exception as err:
         print(err)
+""" if I encounter an error, I send an email indicating the error encountered"""
 except Exception as exc:
     send_mail.send_err(exc)
     print("une erreur", exc)
-    
+""" need to delete the dump created previously"""
 os.system("rm -rf /home/backup")
 
 """ the first variable correspond at ur user and the second at the time of execution cron"""
