9d066e0954fd4d096e12417d7e75411b61eb7a77
AxlMi <47877419+AxlMi@users.noreply.github.com>
2020-09-28 02:13:35 +0200

    Update confidential.py

--- confidential.py
+++ confidential.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+# here, you will find all the variables to be filled for the correct functioning of the script.
+# you can read the README to know how to fill in the variables
 
 aws_key = ''
 aws_secret = ''
